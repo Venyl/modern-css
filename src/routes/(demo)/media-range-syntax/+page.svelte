16f2e782648b3979e1da2dff6b193aa45cc6d240
Venyl <zexe2121@gmail.com>
2025-06-10 16:34:01 +0200

    add the media query range syntax feature page

---
+++ src/routes/(demo)/media-range-syntax/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Codeblock from '$lib/components/Codeblock.svelte';
+	import { browserAvailability } from '$lib/stores.svelte';
+
+	browserAvailability.href = 'https://caniuse.com/css-media-range-syntax';
+</script>
+
+<Codeblock>
+	{#snippet code()}
+		<div style:color="var(--gray-6)">// Przykładowe zastosowanie</div>
+		<div>@media (width &lt; 768px) {'{ }'}</div>
+		<div>@media (768px &lt; width &lt; 1024px) {'{ }'}</div>
+		<div>@media (1000px =&gt; height) {'{ }'}</div>
+	{/snippet}
+</Codeblock>
