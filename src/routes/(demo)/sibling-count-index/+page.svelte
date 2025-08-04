9ea667a59b841f54b88bef197942c9eb7b86414d
Venyl <zexe2121@gmail.com>
2025-08-04 20:55:28 +0200

    build sibling-count(), sibling-index() demo

---
+++ src/routes/(demo)/sibling-count-index/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { browserAvailability } from '$lib/stores.svelte';
+
+	browserAvailability.href =
+		'https://caniuse.com/mdn-css_types_sibling-count,mdn-css_types_sibling-index';
+</script>
+
+<section>
+	<div>Mój <code>sibling-index()</code> to 1, a mój <code>sibling-count()</code> to 3</div>
+	<div>Mój <code>sibling-index()</code> to 2, a mój <code>sibling-count()</code> to 3</div>
+	<div>Mój <code>sibling-index()</code> to 3, a mój <code>sibling-count()</code> to 3</div>
+</section>
+
+<style>
+	section {
+		outline: 2px solid black;
+		padding: var(--size-5);
+		display: grid;
+		gap: var(--size-3);
+	}
+
+	div {
+		padding: var(--size-2);
+		outline: 1px dashed var(--gray-9);
+	}
+</style>
